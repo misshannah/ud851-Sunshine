9d70e7393c9b4ada0f4cca2eb7d0b1e67ba664dd
Hannah Olukoye <hannaholukoye@gmail.com>
2018-06-04 23:17:30 +0300

    Added share options in menu

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,14 +2,20 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
+
     private String mForecast;
+    String title = "Share Weather Details";
     private TextView mWeatherDisplay;
 
     @Override
@@ -29,6 +35,19 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (3) Create a menu with an item with id of action_share
-    // TODO (4) Display the menu and implement the forecast sharing functionality
+    // DONE (3) Create a menu with an item with id of action_share
+    // DONE (4) Display the menu and implement the forecast sharing functionality
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+               MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(createShareForecastIntent());
+               return true;
+    }
+    private Intent createShareForecastIntent() {
+                Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                                .setType("text/plain")
+                                .setText(mForecast + title).getIntent();
+                return shareIntent;
+            }
 }
